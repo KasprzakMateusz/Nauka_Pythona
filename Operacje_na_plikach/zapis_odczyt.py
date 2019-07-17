eea1e12ec30599f723b713273d0757ed4b0963bc
KasprzakMateusz <mateuszkasprzak9@gmail.com>
2019-07-17 14:51:24 +0200

    import os

--- Operacje_na_plikach/zapis_odczyt.py
+++ Operacje_na_plikach/zapis_odczyt.py
@@ -4,3 +4,27 @@ print("-"*30)
 path = os.getcwd()
 print(path)
 print("-"*30)
+
+print("Chcesz stworzyć jakiś plik? Podaj jego nazwę: ")
+file = input("Nazwa pliku: ")
+content = input("Treść pliku: ")
+
+plik = open(file,"w").write(content)
+
+
+print("Wyciąganie treści z pliku")
+
+print("nazwa pliku: {}".format(file))
+print("Treść pliku: {}".format(open(file,'r').read()))
+
+
+wybor = input("chcesz skasować plik? (tak / nie)")
+
+if wybor == "tak":
+    os.remove(file)
+    print("skasowano plik")
+elif wybor == "nie":
+    print("nie skasowano pliku")
+else:
+    print("błędna komenda")
+
