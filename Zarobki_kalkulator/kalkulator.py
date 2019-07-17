19ba48f1d14c5d19ddd29d73e43a8756c9879e82
KasprzakMateusz <mateuszkasprzak9@gmail.com>
2019-07-17 20:27:01 +0200

    update

--- Zarobki_kalkulator/kalkulator.py
+++ Zarobki_kalkulator/kalkulator.py
@@ -1,16 +1,11 @@
-print("-"*30)
-
 days = int(input("Ilość dni pracujących"))
 zarobki = int(input("Zarobki neeto"))
+
 stawka = (zarobki / days / 8)
 ilosc_nadgodzin = int(input("Podaj ilosc nadgodzin"))
 
 print("-"*30)
 print("-"*30)
-print("-"*30)
-print("-"*30)
-
-
 
 print((ilosc_nadgodzin*stawka)*2)
 
