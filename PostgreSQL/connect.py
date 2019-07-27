40e790aa479e373ac1a97d129ded0bd34dd75ca4
KasprzakMateusz <mateuszkasprzak9@gmail.com>
2019-07-27 13:01:40 +0200

    PostgreSQL

--- PostgreSQL/connect.py
+++ PostgreSQL/connect.py
@@ -5,17 +5,29 @@ connection = psycopg2.connect(user = "mateusz",
                                   host = "127.0.0.1",
                                   port = "5433",
                                   database = "nauka")
-on = False
+
+on = True
+add = True
 if on:
     cursor = connection.cursor()
-    model = input("Model: ")
-    price = input("Price: ")
     create_table = "CREATE TABLE IF NOT EXISTS smartphones (id SERIAL PRIMARY KEY, model TEXT, price TEXT)"
-    values = "INSERT INTO smartphones (model, price) VALUES ('"+model+"','"+price+"')"
-
     cursor.execute(create_table)
-    cursor.execute(values)
-    connection.commit()
+    model = input("Model: ")
+    cursor.execute("SELECT model FROM smartphones")
+    records = cursor.fetchall()
+    b = 0
+    for record in records:
+        if model == record[b]:
+            print("Taki model już istnieje")
+            add = False
+            cursor.close()
+            break
+
+    if add:
+        price = input("Price: ")
+        values = "INSERT INTO smartphones (model, price) VALUES ('"+model+"','"+price+"')"
+        cursor.execute(values)
+        connection.commit()
 
 cursor = connection.cursor()
 cursor.execute("SELECT * FROM smartphones")
