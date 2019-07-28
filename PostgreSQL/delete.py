39768450ed179206458e69895992a811a16b9ed2
KasprzakMateusz <mateuszkasprzak9@gmail.com>
2019-07-28 11:30:55 +0200

    PostgreSQL

--- PostgreSQL/delete.py
+++ PostgreSQL/delete.py
@@ -1,11 +1,12 @@
 from db import connection
 
 cursor = connection.cursor()
-cursor.execute("DELETE FROM budget WHERE id = 4")
+cursor.execute("DELETE FROM budget WHERE id = 12")
 connection.commit()
 cursor.execute("SELECT * FROM budget")
 results = cursor.fetchall()
 
-print(results)
+for result in results:
+    print(result)
 
 
